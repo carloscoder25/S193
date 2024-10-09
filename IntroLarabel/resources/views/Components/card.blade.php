1f61ec49ad2210d4f1cd52b19f0e724b04e477fa
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2024-10-09 17:50:16 -0600

    Update card.blade.php

--- IntroLarabel/resources/views/Components/card.blade.php
+++ IntroLarabel/resources/views/Components/card.blade.php
@@ -1,28 +1,20 @@
-<?php
-
+< 2php
 namespace App\View\Components;
-
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-
-class Card extends Component
-{
-    public $encabezado;
-    public $titulo;
-    public $textoBoton;
-    public function __construct($encabezado,$titulo,$textoBoton)
-    {
-        $this->encabezado=$encabezado;
-        $this->titulo=$titulo;
-        $this->textoBoton=$textoBoton;
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return view('components.card');
-    }
-}
+4 references | D implementations
+class card extends Component
+1 reference public fencabezado;
+1 reference public stitulo;
+1 reference public StextoBoton;
+0 references | 0 overrides public function
+construct(Sencabezado, Stitulo, StextoBoton)
+§this-›encabezado=$encabezado;
+Sthis-›titulo-Stitulo;
+sthis-›textoroton=$textoBoton;
+Get the view / contents that represent the component.
+0 references | 0 overrides public function render (): view|closure|string
+return view view:
+"components. card*);
+Chat
