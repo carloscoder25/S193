10864f2001bab2d1423f99627b186be6f8918a29
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2024-10-17 17:39:48 -0600

    Paso 3 creacion de vista portada

--- practicaRepaso/resources/views/Portada.blade.php
+++ practicaRepaso/resources/views/Portada.blade.php
@@ -1,32 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite(['resources/js/app.js'])
+        <title>Portada</title>
+        <style>
+            h1{
+                color: green;
+            }
+        </style>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
+    </head>
+    <body>
+        <div class="d-flex flex-column justify-content-center align-items-center full-height">
+            <h1 class="display-1" style="color:greenyellow">Bienvenido a la aplicación</h1>
+
+        </div>
     
-    <title>Inicio</title>
-    <style>
-        body, html {
-            height: 100%;
-            
-        }
-        .full-height {
-            height: 100vh;
-            background-color: lightslategrey;
-        }
-        
-    </style>
-</head>
-<body>
-    <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
-        <h1 class="display-1"> Bienvenido al Repaso de Larabel</h1>
-        <h2 class="display-4"> Creado por Carlos Saúl Muñoz Ibarra</h2>
-        <p>Presiona el boton para iniciar....</p>
-
-        <a href="/Repaso" class="btn btn-success" style="background-color: yellowgreen; color: white;"> Ir al Repaso </a>     
-        {{-- <a href="" class="btn btn-danger"> Ir al Registro </a> --}}
-    </div>
-</body>
+    </body>
 </html>
