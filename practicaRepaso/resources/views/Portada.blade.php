541f949ec0baf96be5604c9f7e3c0755ba3bc815
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2024-10-17 17:52:40 -0600

    Paso 4 boton direccionado vista

--- practicaRepaso/resources/views/Portada.blade.php
+++ practicaRepaso/resources/views/Portada.blade.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>Portada</title>
-        <style>
-            h1{
-                color: green;
-            }
-        </style>
+       
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <style>
+            body,html{
+                height: 100%
+            }
+            .full-height{
+                height: 100vh;
+            }
+        </style> 
        
     </head>
-    <body>
-        <div class="d-flex flex-column justify-content-center align-items-center full-height">
-            <h1 class="display-1" style="color:greenyellow">Bienvenido a la aplicación</h1>
+    <body >
+        <div class="d-flex flex-column justify-content-center align-items-center full-height" style="background-color: lightslategray">
+            <h1 class="display-1" style="color:greenyellow">Bienvenido al Repaso</h1>
+            <h3 class="display-4" style="color:greenyellow">Creado Por Carlos Saúl Muñoz Ibarra</h3>
+            <p style="color:greenyellow">Grupo S193</p>
+            <a href="/Repaso" class="btn btn-success">Ir al Repaso</a>
 
         </div>
     
--- practicaRepaso/routes/web.php
+++ practicaRepaso/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Portada');
 });
+
+Route::get('/Repaso', function () {
+    return view('Repaso');
+});
